10493f1028f5e948b18ea8ddc47aa6b467955084
madiouni mohamed <madiounimohamed92@gmail.com>
2020-11-24 16:33:48 +0100

    update

---
+++ client/src/component/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "../navbar.css"
+function Navbar() {
+    return (
+        <div className="">
+         
+                <div className="row">
+                    <div className="col-4">
+<img src="/vibtic.png" alt="vibtic"/>
+                    </div>
+                    <div className="col-8">
+
+                    </div>
+                </div>
+
+      
+        </div>
+    )
+}
+
+export default Navbar
